7a693a6f0dd054f6941a1308ca3086ebf043ce39
DjamalDixneuf <clydedjamaldine@gmail.com>
2024-11-18 12:52:35 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,6 +3,8 @@ const serverless = require('serverless-http')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const express = require('express');
+const app = express();
 
 server.use(middlewares)
 server.use(router)
