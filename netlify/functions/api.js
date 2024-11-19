031e78adc98d8598bfe084f4a11eeb4f9e24ac07
DjamalDixneuf <clydedjamaldine@gmail.com>
2024-11-19 11:02:52 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,14 +2,11 @@ const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  console.log(`Requête reçue: ${req.method} ${req.path}`);
-  next();
-});
+
 let movies = [];
 
 app.get('/api/movies', (req, res) => {
